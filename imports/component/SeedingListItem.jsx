fc1771b861263e13f5e84dd2ea050955fe18f809
kichimo <>
2020-01-10 17:07:43 +0800

    Problem: cant stop seeding
    Solution: allow user to click on the item and stop seeding

--- imports/component/SeedingListItem.jsx
+++ imports/component/SeedingListItem.jsx
@@ -1,14 +1,17 @@
-import React, {useState} from 'react'
-import { ListItem } from 'react-onsenui'
+import React, {useState, useEffect} from 'react'
+import { ListItem, Icon } from 'react-onsenui'
 
 export default function SeedingListItem({ vid }) {
     
     const [seedingState, setSeedingState] = useState(false)
-    const [seedingSpeed, setSeedingSpeed] = useState(0)
-    var client = new window.WebTorrent()
+    const [uploadSpeed, setUploadSpeed] = useState(0)
+    const [downloadSpeed, setDownloadSpeed] = useState(0)
+    const [client, setClient] = useState(new window.WebTorrent())
+
     function startseeding() {
         setSeedingState(true)
-        setSeedingSpeed(0)
+        setUploadSpeed(0)
+        setDownloadSpeed(0)
         var torrentId = vid.magnetUri
         client.on('error', err => {
             console.log('[+] Webtorrent error: ' + err.message);
@@ -17,21 +20,40 @@ export default function SeedingListItem({ vid }) {
         client.add(torrentId, (torrent) => {
             console.log('Client is downloading:', torrent.infoHash)
             torrent.on('upload', function (bytes) {
-                console.log(torrent.uploadSpeed)
-                setSeedingSpeed(torrent.uploadSpeed )
+                setUploadSpeed(torrent.uploadSpeed )
             })
+            torrent.on('download', function (bytes) {
+                setDownloadSpeed(torrent.downloadSpeed )
+            })
+        })
+    }
+    function stopSeeding(){
+        setSeedingState(false)
+        setUploadSpeed(0)
+        setDownloadSpeed(0)
+        client.remove(vid.magnetUri, () => {
+            console.log('stop seeding ' + vid.title)
         })
     }
 
     if (seedingState) {
         return <ListItem
+            tappable
             style={{
                 color: "grey"
             }}
+            onClick={()=>
+                stopSeeding()
+            }
         >
             <div className="left">{vid.title}</div>
             <div className="center"></div>
-            <div className="right">{seedingSpeed / 1000} kb/s</div>
+            <div className="right">
+            <Icon icon="arrow-down"/>
+            {downloadSpeed / 1000} kb/s
+            <Icon icon="arrow-up"/>
+            {uploadSpeed / 1000} kb/s
+            </div>
         </ListItem>
     }
     return <ListItem
